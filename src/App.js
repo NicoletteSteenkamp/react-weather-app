da4d989193095d4cd8dad7cab573fa30b99e461e
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-12 14:28:50 +0200

    added a new component, added bootstrap and updated it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react"
+import Weather from "./Weather"
 import './App.css';
 
  export default function App() {
   return (
     
     <div className="App">
+      <div className="container">
       <h1>Weather App </h1>
+      <Weather />
     <footer>
       This app is coded by Nicolette Steenkamp and is 
     <a href="https://github.com/NicoletteSteenkamp/weather-app-project" target="blank"> open-sourced on Github </a>
     </footer> 
+    </div>
     </div>
   );
 }
