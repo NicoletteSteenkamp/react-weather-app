52229922995d6d2f1973556928cedfebd446c97d
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-12 13:48:21 +0200

    Added github link to the fiile

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
  export default function App() {
   return (
+    
     <div className="App">Weather App
-      
+    <footer>
+      This app is coded by Nicolette Steenkamp and is 
+    <a href="https://github.com/NicoletteSteenkamp/weather-app-project" target="blank"> open-sourced on Github </a>
+    </footer> 
     </div>
   );
 }
