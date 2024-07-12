03dec3299b3cee86558b6776f9585d97482ff550
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-12 13:52:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
  export default function App() {
   return (
     
-    <div className="App">Weather App
+    <div className="App">
+      <h1>Weather App </h1>
     <footer>
       This app is coded by Nicolette Steenkamp and is 
     <a href="https://github.com/NicoletteSteenkamp/weather-app-project" target="blank"> open-sourced on Github </a>
