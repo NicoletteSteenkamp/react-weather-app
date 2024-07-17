3c79c04fe80fb43a34bc9369015d727a306c68ba
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-17 16:07:43 +0600

    added htm;

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import './App.css';
     
     <div className="App">
       <div className="container">
-      <h1>Weather App </h1>
+      
       <Weather />
     <footer>
-      This app is coded by Nicolette Steenkamp and is 
-    <a href="https://github.com/NicoletteSteenkamp/weather-app-project" target="blank"> open-sourced on Github </a>
+      This app is coded by <a href="https://github.com/NicoletteSteenkamp">Nicolette Steenkamp</a> and is open-sourced on{" "}
+    <a href="https://github.com/NicoletteSteenkamp/weather-app-project" target="blank">Github </a>
     </footer> 
     </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,35 @@
 import React from "react";
-
+import "./Weather.css";
 export default function Weather(){
-    return <div className="Weather">Hello from Weather </div>
+    return <div className="Weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+            <input type="search" placeholder="Search for a city.." className="form-contol"/>
+            
+            </div>
+            <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary"/>
+            </div>
+            </div>
+        </form>
+        <h1>Boksburg</h1>
+    <ul>
+        <li>Wednesday 11:43</li>
+        <li>Sunny</li>
+    </ul>
+    <div className="row">
+        <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"/>
+            20°C
+        </div>
+        <div className="col-6">
+            <ul>
+                <li>Precipitation: 0%</li>
+                <li>Humidity: 17%</li>
+                <li>Wind: 8 km/h</li>
+            </ul>
+        </div>
+    </div>
+    </div>
 }
