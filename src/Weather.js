fa91f4a87101c9e00d5f46b9fe191b91f49c7a0c
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-17 13:31:44 +0200

    added styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,11 @@ export default function Weather(){
         <form>
             <div className="row">
                 <div className="col-9">
-            <input type="search" placeholder="Search for a city.." className="form-contol"/>
+            <input type="search" placeholder="Search for a city.." className="form-contol w-100"autoFocus="on"/>
             
             </div>
             <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary"/>
+            <input type="submit" value="Search" className="btn btn-primary w-100"/>
             </div>
             </div>
         </form>
@@ -18,10 +18,15 @@ export default function Weather(){
         <li>Wednesday 11:43</li>
         <li>Sunny</li>
     </ul>
-    <div className="row">
+    <div className="row mt-3">
         <div className="col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"/>
-            20°C
+            
+            <div className="clearfix">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny" className="float-left"/>
+            <div className="float-left">
+            <span className="temperature">20</span><span className="unit">°C</span>
+        </div>
+        </div>
         </div>
         <div className="col-6">
             <ul>
